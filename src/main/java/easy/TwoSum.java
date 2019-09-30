407f2ef0e278bd2336bf99de6ab775bde4f6928b
wangxin <1065373753@qq.com>
2019-09-30 08:03:01 +0800

    update

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -3,18 +3,31 @@ package easy;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * */
+
 public class TwoSum {
 
     public static void main(String[] args){
         twoSum(new int[]{3, 2, 4}, 6);
 
     }
+
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        //这段应该很好理解，在往map里面插入数据的同时，检测目前map是否已经有达到条件的数，且只会循环一次，不会出现key重叠的情况
-        for (int i = 0 ; i<nums.length ; i++){
-            if (map.containsKey(target - nums[i]))
-                return new int[]{i,map.get(target - nums[i])};
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            }
             map.put(nums[i], i);
         }
         return null;
